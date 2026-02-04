8dfe22bedfba7a7f9733604b3489b8e4d5af1406
jljimenez <jljimenez@aseproda.com>
2026-02-04 20:45:16 +0100

    chore(deploy): rebuild frontend binaries with UI fix [manual-deploy]

--- frontend/build/web/flutter_service_worker.js
+++ frontend/build/web/flutter_service_worker.js
@@ -24,7 +24,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "0b0a3415aad49b6e9bf965ff578614f9
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "8b0592ea7d1dfa6f163bc892fcfec185",
+"flutter_bootstrap.js": "9b72fbd75da4c8d41df035381c066b6e",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
